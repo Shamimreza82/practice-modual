d937a954323625bd405d33a0142ad7a70856e98c
Shamimreza82 <shamimrezaone@gmail.com>
2023-08-27 00:19:16 +0600

    added new code

--- first.js
+++ first.js
@@ -20,30 +20,70 @@
 // }
 
 
+    //  post show on display in site
+
+//    function postData (){
+//     fetch ('https://jsonplaceholder.typicode.com/posts')
+//     .then(res => res.json())
+//     .then(post => postDisplay(post))
+//    }
+//    postData ()
 
 
-   function postData (){
-    fetch ('https://jsonplaceholder.typicode.com/posts')
-    .then(res => res.json())
-    .then(post => postDisplay(post))
-   }
-   postData ()
-
-
-    function postDisplay(post){
-        const section = document.getElementById ('show-post');
-        for (const items of post){
-            console.log (items)
-            const div = document.createElement ('div'); 
-            div.innerHTML = ` 
-            <h3> userId:- ${items.userId} </h3> 
-            <h3> ID:- ${items.id} </h3> 
-            <h1> Title:- ${items.title} </h1> 
-            <h1> Body:- ${items.body} </h1> 
-            `; 
-            section.appendChild (div); 
-        }
+//     function postDisplay(post){
+//         const section = document.getElementById ('show-post');
+//         for (const items of post){
+//             console.log (items)
+//             const div = document.createElement ('div'); 
+//             div.innerHTML = ` 
+//             <h3> userId:- ${items.userId} </h3> 
+//             <h3> ID:- ${items.id} </h3> 
+//             <h1> Title:- ${items.title} </h1> 
+//             <h1> Body:- ${items.body} </h1> 
+//             `; 
+//             section.appendChild (div); 
+//         }
             
-    }
+//     }
 
-    postDisplay();
+//     postDisplay();
+
+
+
+    //  Commment Display json 
+
+
+
+        // function comment () {
+        //     fetch ('https://jsonplaceholder.typicode.com/comments')
+        //     .then (res => res.json())
+        //     .then (postComment => commentDisplay(postComment))
+        // }
+        // comment ()
+
+        // function commentDisplay(comm){
+        //     const commments = document.getElementById ('show-post'); 
+        //     for (const items of comm){
+        //         const div = document.createElement ('div'); 
+        //         div.classList.add ('show-post')
+        //         div.innerHTML = `
+        //         <h1>Name:- ${items.name}</h1>
+        //         <h3>ID:- ${items.id}</h3>
+        //         <h4>Body:- ${items.body}</h4>
+        //         <h4>Email:- ${items.email}</h4>
+                
+        //         `; 
+        //         commments.appendChild(div);
+        //         console.log (items)
+        //     }
+
+        // }
+    
+        // commentDisplay ();
+
+
+
+
+
+
+        
